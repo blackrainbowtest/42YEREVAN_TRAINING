a5e4f379a7444d4198bb5565cfe6b3b407194d7f
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-12-08 15:38:38 +0400

    trainin

--- ft_popen2.c
+++ ft_popen2.c
@@ -29,4 +29,31 @@ int	ft_popen(const char *file, char *const argv[], char type)
 		close(fds[1]);
 		return (-1);
 	}
+	if (pid == 0)
+	{
+		if (type == 'r')
+		{
+			if (dup2(fds[1], STDOUT_FILENO) == -1)
+				exit (1);
+		}
+		else
+		{
+			if (dup2(fds[0], STDIN_FILENO) == -1)
+				exit (1);
+		}
+		close(fds[0]);
+		close(fds[1]);
+		execvp(file, argv);
+		exit(1);
+	}
+	if (type == 'r')
+	{
+		close(fds[1]);
+		return (fds[0]);
+	}
+	else
+	{
+		close(fds[0]);
+		return (fds[1]);
+	}
 }
