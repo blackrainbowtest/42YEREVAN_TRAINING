b0d90f74d58b03ed16d71783caf22552eb94cb8d
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-12-10 17:01:17 +0400

    first update

--- picoshell/picoshell1.c
+++ picoshell/picoshell1.c
@@ -17,9 +17,46 @@ int picoshell(char **cmds[])
 	i = 0;
 	while (cmds[i])
 	{
+		if (cmds[i + 1] && pipe(fds) == -1)
+			return (-1);
+		pid = fork();
+		if (pid == -1)
+		{
+			if (cmds[i + 1])
+			{
+				close(fds[0]);
+				close(fds[1]);
+			}
+			return (1);
+		}
+		if (pid == 0)
+		{
+			if (prev_fd !=close -1)
+			{
+				if (dup2(prev_fd, STDIN_FILENO) == -1)
+					exit (1);
+				close(prev_fd);
+			}
+			if (cmds[i + 1])
+			{
+				close(fds[0]);
+				if (dup2(fds[1], STDOUT_FILENO) == -1)
+					exit (-1);
+				close(fds[1]); 
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+		if (prev_fd != -1)
+			close(prev_fd);
+		if (cmds[i + 1])
+		{
+			close(fds[1]);
+			prev_fd = fds[0]
+		}
 		i++;
 	}
-	while (wait(&status) != 1)
+	while (wait(&status) != -1)
 	{
 		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 			exit_code = 1;
@@ -27,9 +64,15 @@ int picoshell(char **cmds[])
 	return (exit_code);
 }
 
-int	main(int argc, char **argv)
+int main(void)
 {
-	if (argc < 2)
-		return (1);
-	printf("pico = %d\n", picoshell(&argv));
+    char *cmd1[] = {"ls", "-l", NULL};
+    char *cmd2[] = {"grep", "c", NULL};
+    char *cmd3[] = {"wc", "-l", NULL};
+
+    char **cmds[] = {cmd1, cmd2, cmd3, NULL};
+
+    int res = picoshell(cmds);
+    printf("exit = %d\n", res);
+    return 0;
 }
