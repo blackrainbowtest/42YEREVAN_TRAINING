8d139f5886c6a522cc4407cddcab0b379e6d019c
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-12-11 16:00:54 +0400

    update

--- picoshell/picoshell3.c
+++ picoshell/picoshell3.c
@@ -3,23 +3,24 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int picoshell(char **cmds[])
+int	picoshell(char **cmds[])
 {
-	int		exit_code;
-	int		i;
-	int		status;
-	int		prev_fd;
-	int		fds[2];
 	pid_t	pid;
+	int		i;
+	int		exit_code;
+	int		status;
+	int		fds[2];
+	int		prev_fd;
 
 	exit_code = 0;
-	prev_fd = -1;
 	i = 0;
-	while(cmds[i])
+	prev_fd = -1;
+	while (cmds[i])
 	{
+
 		i++;
 	}
-	while (wait(&status) != -1)
+	while (wait(&status != -1))
 	{
 		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 			exit_code = 1;
@@ -29,12 +30,12 @@ int picoshell(char **cmds[])
 
 int	main(void)
 {
-	char *cmd1[] = {"ls", "-l", NULL};
-	char *cmd2[] = {"grep", "c", NULL};
-	char *cmd3[] = {"wc", "-l", NULL};
+	char	*cmd1[] = {"ls", "-l", NULL};
+	char	*cmd2[] = {"grep", "c", NULL};
+	char	*cmd3[] = {"wc", "-l", NULL};
 
-	char **cmds[] = {cmd1, cmd2, cmd3, NULL};
+	char	**cmds[] = {cmd1, cmd2, cmd3, NULL};
 	int	res = picoshell(cmds);
-	printf("exit_code %d\n", res);
+	printf("exit_code: %d\n", res);
 	return (0);
 }
