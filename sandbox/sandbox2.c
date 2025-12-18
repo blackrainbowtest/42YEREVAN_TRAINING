a3bd42e69ab326dc6b6e83144c3f6425c26b87cf
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-12-18 17:58:07 +0400

    ւպդատե

--- sandbox/sandbox2.c
+++ sandbox/sandbox2.c
@@ -5,12 +5,17 @@
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-static pid_t	child_pid;
+static pid_t child_pid;
 
-void alarm_handler(int	sig)
+void alarm_handler(int sig)
 {
-	void(sig);
+	(void)sig;
+}
+
+int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+{
+
 }
