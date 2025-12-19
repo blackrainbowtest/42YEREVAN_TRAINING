ea3bab412073b17f946307b9477abaf7ee648713
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-12-19 15:36:25 +0400

    refactor

--- sandbox/sandbox2.c
+++ sandbox/sandbox2.c
@@ -8,14 +8,44 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-static	pid_t child_pid;
+static pid_t	child_pid;
 
-void	alarm_handler(int sig)
+void alarm_handler(int	sig)
 {
 	(void)sig;
 }
 
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
+	pid_t	pid;
+	struct sigaction sa;
+	int		status;
 
+	sa.sa_handler = alarm_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGALRM, &sa, NULL);
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	if (pid == 0)
+	{
+		f();
+		_exit (0);
+	}
+	child_pid = pid;
+	alarm(timeout);
+	if (waitpid(pid, &status, 0) == -1)
+	{
+
+	}
+	if (WIFEXITED(status))
+	{
+
+	}
+	if (WIFSIGNALED(status))
+	{
+		
+	}
+	return (-1);
 }
