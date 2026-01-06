869dc86a78fc0ee866b3816db01e3315ae99d822
blackrainbowtest <araqelyan.aram.96@gmail.com>
2026-01-06 21:53:34 +0400

    add

---
+++ vbc2/vbc2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct node {
+	enum { ADD, MULTI, VAL} type;
+	int val;
+	struct node *l;
+	struct node *r;
+} node;
+
+node *new_node(node n)
+{
+	node *ret = calloc(1, sizeof(node));
+	if (!ret)
+		return (NULL);
+	*ret = n;
+	return (ret);
+}
