451bb090395692b10d7071f48ec8ef27b27d289b
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-01-16 09:42:40 +0400

    refactor

--- ✔ picoshell/picoshell1.c
+++ ✔ picoshell/picoshell1.c
@@ -1,12 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <signal.h>
-#include <errno.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <string.h>
 
 int	picoshell(char **cmds[])
 {
