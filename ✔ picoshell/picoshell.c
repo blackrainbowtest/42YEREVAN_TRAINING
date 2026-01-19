0fc4307d90100a35337a060fdeb54c04b265e608
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-01-19 11:47:07 +0400

    refactor: updated

--- ✔ picoshell/picoshell.c
+++ ✔ picoshell/picoshell.c
@@ -3,65 +3,72 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int picoshell(char **cmds[])
+int	picoshell(char **cmds[])
 {
-    pid_t pid;
-    int pipefd[2];      // Pipe actual
-    int prev_fd = -1;   // Descriptor del pipe anterior
-    int status;
-    int exit_code = 0;
-    int i = 0;
-    
-    while (cmds[i])
-    {
-        if (cmds[i + 1] && pipe(pipefd) == -1)
-            return 1;
-        pid = fork();
-        if (pid == -1)
-        {
-            if (cmds[i + 1])
-            {
-                close(pipefd[0]);
-                close(pipefd[1]);
-                return 1;
-            }
-        }
-        if (pid == 0)  // PROCESO HIJO
-        {
-            if (prev_fd != -1)
-            {
-                if (dup2(prev_fd, STDIN_FILENO) == -1)
-                    exit(1);
-                close(prev_fd);
-            }
-            if (cmds[i + 1])
-            {
-                close(pipefd[0]);  // Cerrar read end (no lo necesitamos)
-                if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-                    exit(1);
-                close(pipefd[1]);
-            }
-            execvp(cmds[i][0], cmds[i]);
-            exit(1);  // Solo se ejecuta si execvp falla
-        }
-        if (prev_fd != -1)
-            close(prev_fd);
-        if (cmds[i + 1])
-        {
-            close(pipefd[1]);     // Cerrar write end
-            prev_fd = pipefd[0];  // Guardar read end para siguiente iteración
-        }
-        i++;
-    }
-    while (wait(&status) != -1)
-    {
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-            exit_code = 1;
-    }
-    return exit_code;
+	int		i = 0;
+	int		exit_code = 0;
+	int		prev_fds = -1;
+	int		status;
+	int		fds[2];
+	pid_t	pid;
+
+	while (cmds[i])
+	{
+		if (cmds[i + 1] && pipe(fds) == -1)
+			return (1);
+		pid = fork();
+		if (pid == -1)
+		{
+			if (cmds[i + 1])
+			{
+				close(fds[0]);
+				close(fds[1]);
+			}
+			return (1);
+		}
+		if (pid == 0)
+		{
+			if (prev_fds != -1)
+			{
+				if (dup2(prev_fds, STDIN_FILENO) == -1)
+					exit (1);
+				close(prev_fds);
+			}
+			if (cmds[i + 1])
+			{
+				close(fds[0]);
+				if (dup2(fds[1], STDOUT_FILENO) == -1)
+					exit (1);
+				close(fds[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit (1);
+		}
+		if (prev_fds != -1)
+			close(prev_fds);
+		if (cmds[i + 1])
+		{
+			close(fds[1]);
+			prev_fds = fds[0];
+		}
+		i++;
+	}
+	while (wait(&status) != -1)
+	{
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			exit_code = 1;
+	}
+	return (exit_code);
 }
 
-int main(int argc, char **argv)
+int	main(void)
 {
-	printf("pico = %d\n", picoshell(&argv));
+	char	*cmd1[] = {"ls", "-l", NULL};
+	char	*cmd2[] = {"grep", "c", NULL};
+	char	*cmd3[] = {"wc", "-l", NULL};
+
+	char	**cmds[] = {cmd1, cmd2, cmd3, NULL};
+	int		res = picoshell(cmds);
+	printf("Exit_code %d\n", res);
+	return (0);
 }
