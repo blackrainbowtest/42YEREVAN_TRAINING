9f9f5d9431b56d174b16661aa37ae36333f5c9a9
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2026-01-30 15:43:26 +0400

    refactor

---
+++ picoshell/picoshell4.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int	picoshell(char **cmds[])
+{
+	int i = -1;
+	int prev_fds = -1;
+	int fds[2];
+	pid_t pid;
+
+	while (cmds[++i])
+	{
+		if (cmds[i + 1] && pipe(fds) == -1)
+			return (1);
+		pid = fork();
+		if (pid == -1)
+		{
+			if (cmds[i + 1])
+			{
+				close(fds[0]);
+				close(fds[1]);
+			}
+			if (prev_fds)
+				close(prev_fds);
+			return (1);
+		}
+		if (pid == 0)
+		{
+			if (prev_fds != -1)
+			{
+				if (dup2(prev_fds, 0) == -1)
+					exit (1);
+				close(prev_fds);
+			}
+			if (cmds[i + 1])
+			{
+				close(fds[0]);
+				if (dup2(fds[1], 1) == -1)
+					exit(1);
+				close(fds[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit (1);
+		}
+		if (prev_fds != -1)
+			close(prev_fds);
+		if (cmds[i + 1])
+		{
+			close(fds[1]);
+			prev_fds = fds[0];
+		}
+	}
+	while (wait(NULL) > 0)
+		;
+	return (0);
+}
+
+int main(void)
+{
+	char *cmd1[] = {"ls", "-l", NULL};
+	char *cmd2[] = {"grep", "c", NULL};
+	char *cmd3[] = {"wc", "-l", NULL};
+
+	char **cmds[] = {cmd1, cmd2, cmd3, NULL};
+	int res = picoshell(cmds);
+	printf("exit_code: %d\n", res);
+	return (0);
+}
--- vbc/vbc1.c
+++ vbc/vbc1.c
@@ -182,7 +182,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		return (1);
 	char *input = argv[1];
-	node *tree = parse_expression(&argv[1]);
+	node *tree = parse_expression(&input);
 	if (!tree)
 		return (1);
 	printf("%d\n", eval_tree(tree));
